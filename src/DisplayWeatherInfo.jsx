d95ce8d869a66b3a1f835b4a64faaa14f226ea45
Sam2963 <shaikhsamir2963@gmail.com>
2026-03-20 08:34:15 +0530

    Removed API key for security

--- src/DisplayWeatherInfo.jsx
+++ src/DisplayWeatherInfo.jsx
@@ -29,7 +29,7 @@ function DisplayWeatherInfo() {
     if(!newCity.trim()) return;
     setError("");
 
-    const apiKey = "f941993b1744af681997805812bcbf16";
+    const apiKey = "YOUR_API_KEY";
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&appid=${apiKey}&units=metric`;
 
     try {
